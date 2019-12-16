5da6a0365be1e6965da9ff66aa3e9ee3e33bad50
Ross Nicholls <rjnicholls79@icloud.com>
2019-12-16 12:30:39 +0000

    second commit
    second commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,50 @@
 <template>
-  <div id="app">
-    <div id="nav">
+<div id="app" class="">
+  <!--<div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
-    <router-view/>
-  </div>
+    <router-view/>-->
+    <section class="log-in" v-if="isShow['Log']">
+      <h2>please sign in</h2>
+    </section>
+  <section class="signIn" v-if="isShow['Sign']">
+    <h2 class="text-center">please sign in</h2>
+    <div class="mx-auto d-block sign-arrow" @click="isShow['Log'] = !isShow['Log']; isShow['Sign'] = !isShow['Sign']"><i class="fas fa-chevron-down"></i></div>
+  </section>
+  <section class="container">
+    <section class="main-illustration">
+      <img alt="House" src="./assets/Img_Asset.svg" class="mx-auto d-block">
+    </section><!-- End Main Illustration -->
+    <section class="logo">
+      <img alt="House" src="./assets/Logo_Asset.svg" class="mx-auto d-block">
+    </section><!-- End logo -->
+    <h1 class="text-center mt-2">welcome to our online referral portal</h1>
+  </section><!-- End Container -->
+</div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+// @ is an alias to /src
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+export default {
+  name: 'home',
+  components: {
+  },
+  data(){
+    return{
+      isShow:{
+        'Sign': true,
+        'Log': false,
+      }
     }
-  }
+  },
+
+
 }
+</script>
+
+<style lang="scss">
+@import "./styles/universal.scss";
+@import "./styles/log-in.scss";
 </style>
