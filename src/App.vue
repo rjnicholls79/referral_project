1903a619d7f2acf3c32525c4c8b67cfe018cfb9c
Ross Nicholls <rjnicholls79@icloud.com>
2019-12-16 14:49:13 +0000

    fsdfs
    sfafdsa

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,14 @@
       <router-link to="/about">About</router-link>
     </div>
     <router-view/>-->
+    <transition name="fade">
     <section class="log-in" v-if="isShow['Log']">
       <h2>please sign in</h2>
+      <input type="input" placeholder="User Name">
+      <input type="password" placeholder="Password">
+      <p>Lost Your Credentials</p>
     </section>
+  </transition>
   <section class="signIn" v-if="isShow['Sign']">
     <h2 class="text-center">please sign in</h2>
     <div class="mx-auto d-block sign-arrow" @click="isShow['Log'] = !isShow['Log']; isShow['Sign'] = !isShow['Sign']"><i class="fas fa-chevron-down"></i></div>
